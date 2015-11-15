9046589a81ea5f5c75dae9e7c79fbb5ad8646bab
strongzensam <strongzensam@gmail.com>
2015-11-15 11:31:51 -0800

    Bootstrap alerts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,36 @@
 class ProductsController < ApplicationController
 
-  def productslist
+  def index
     @products = Product.all
   end
+  def new
+  end
+  def create
+     @new_product = Product.create(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+     flash[:success] = "Product Created"
+     redirect_to "/products/#{@new_product.id}"
+  end
+  def show 
+    id = params[:id]
+    @product = Product.find_by(id: id)
+  end
+  def edit
+    id = params[:id]
+    @product = Product.find_by(id: id)
+  end
+  def update
+    id = params[:id]
+    @product = Product.find_by(id: id)
+    @product.update(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    flash[:success] = "Product Updated"
+    redirect_to "/products/#{@product.id}"
 
+  end
+  def destroy
+    id = params[:id]
+    product = Product.find_by(id: id)
+    product.destroy
+    flash[:danger] = "Product Destroyed"
+    redirect_to "/products"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get '/products', to: 'products#productslist'
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  post '/products', to: 'products#create'
+  get '/products/:id', to: 'products#show'
+  get '/products/:id/edit', to: 'products#edit'
+  patch '/products/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
