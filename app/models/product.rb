ae8c07af3fe5ea967226f4e87b24b9025f64069f
strongzensam <strongzensam@gmail.com>
2015-11-16 21:29:17 -0800

    Price is now decimal

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ActiveRecord::Base
+  def price_without_dollarsign
+    if price.include?("$")
+      # p "Boom"
+      price_split =  price.split("")
+      price_split = price_split[1..-1]
+      price_split.join("")
+    else
+      price
+    end
+  end
+  def sale_message
+    if price_without_dollarsign.to_i < 2
+      return "Discount Item!"
+    else
+      return "On sale"
+    end
+  end
+  def tax
+    tax = price_without_dollarsign.to_i * 0.09
+  end
+  def total
+    total = price_without_dollarsign.to_i + tax
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110050017) do
+ActiveRecord::Schema.define(version: 20151117051455) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "price"
+    t.decimal  "price",       precision: 5, scale: 2
     t.text     "image"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
 end
