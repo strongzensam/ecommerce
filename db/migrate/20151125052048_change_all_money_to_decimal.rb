cfcf15f0ea691d21928aebc4debde2cf4b739ae8
strongzensam <strongzensam@gmail.com>
2015-11-25 18:14:15 -0800

    Additional attempted fix for Heroku compatibility

--- db/migrate/20151125052048_change_all_money_to_decimal.rb
+++ db/migrate/20151125052048_change_all_money_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangeAllMoneyToDecimal < ActiveRecord::Migration
   def change
-    change_column :products, :price, 'integer USING price::numeric(5,2)'
+    change_column :products, :price, 'numeric USING price::numeric(5,2)'
   end
 end
